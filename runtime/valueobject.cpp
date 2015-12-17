f5b15a4111a86a6cfe11fce2f801dba02cca1bf6
Benjamin J Anderson <banderson@mashable.com>
2015-12-16 19:52:01 -0600

    Layout value object

--- runtime/valueobject.cpp
+++ runtime/valueobject.cpp
@@ -1,23 +1,47 @@
 #include "valueobject.hpp"
 
+/*
+
+  ValueObject - Primitive Datatype Storage
+    Object for storing primitive datatypes and assigning a parent class
+
+*/
 Runtime::ValueObject::ValueObject(Runtime::StdClass *klass, boost::variant<int,std::string> value)
 /* TODO: Call Super after runtime is initialized -- : super(i), J(j) etc */
 {
   this->value = value;
 }
 
+/*
+
+  Constructor - String Type
+    Sets the primitive datatype to String -- super("String")
+
+*/
 Runtime::ValueObject::ValueObject(std::string value)
 /* TODO: Call Super after runtime is initialized -- : super(i), J(j) etc */
 {
   this->value = value;
 }
 
+/*
+
+  Constructor - Int Type
+    Sets the primitive datatype to int -- super("Integer")
+
+*/
 Runtime::ValueObject::ValueObject(int value)
 /* TODO: Call Super after runtime is initialized -- : super(i), J(j) etc */
 {
   this->value = value;
 }
 
+/*
+
+  Constructor - Float Type
+    Sets the primitive datatype to float -- super("float")
+
+*/
 Runtime::ValueObject::ValueObject(float value)
 /* TODO: Call Super after runtime is initialized -- : super(i), J(j) etc */
 {
@@ -36,6 +60,12 @@ Runtime::ValueObject::isNil()
   /* TODO */
 }
 
+/*
+
+  getValue()
+    Returns the primitive (native) DataType in C++
+
+*/
 boost::variant<int,std::string>
 Runtime::ValueObject::getValue()
 {
