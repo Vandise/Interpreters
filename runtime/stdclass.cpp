a2cb52c7a287e0657628c082852412cd1fc97329
Benjamin J Anderson <banderson@mashable.com>
2015-12-17 01:49:28 -0600

    Finish Class Structures

--- runtime/stdclass.cpp
+++ runtime/stdclass.cpp
@@ -22,67 +22,89 @@ Runtime::StdClass::construct(std::string name)
 std::string
 Runtime::StdClass::getName()
 {
-  /* TODO */
+  return name;
 }
 
 void
-Runtime::StdClass::setConstant(std::string name, Runtime::Object value)
+Runtime::StdClass::setConstant(std::string name, Runtime::Object *value)
 {
-  /* TODO */
+  constants[name] = value;
+}
+
+Runtime::StdClass*
+Runtime::StdClass::getSuperClass()
+{
+  return superClass;
 }
 
 Runtime::Object*
 Runtime::StdClass::getConstant(std::string name)
 {
-  /* TODO */
+  if(constants.count(name) >= 1)
+    return constants[name];
+  if(superClass)
+    return superClass->getConstant(name);
+  return Lang::Runtime::getNil();
 }
 
 int
 Runtime::StdClass::hasConstant(std::string name)
 {
-  /* TODO */
+  if(constants.count(name) >= 1)
+    return 1;
+  if(superClass)
+    return superClass->hasConstant(name);
+  return 0;
 }
 
 void
 Runtime::StdClass::lookup(std::string name)
 {
-  /* TODO */
+  /* TODO - methods */
 }
 
 int
 Runtime::StdClass::hasMethod(std::string name)
 {
-  /* TODO */
+  /* TODO - methods  */
 }
 
 void
 Runtime::StdClass::addMethod(std::string name, std::string method)
 {
-  /* TODO */
+  /* TODO - methods */
 }
 
-Runtime::Object
+Runtime::Object*
 Runtime::StdClass::newInstance(std::string value)
 {
-  /* TODO */
+  return new Runtime::Object(this);
 }
 
-Runtime::StdClass
+Runtime::StdClass*
 Runtime::StdClass::newSubclass(std::string name)
 {
-  /* TODO */
+  Runtime::StdClass *klass = new Runtime::StdClass(name, this);
+  Lang::Runtime::getObjectClass()->setConstant(name, klass);
+  return klass;
 }
 
-Runtime::StdClass
+Runtime::StdClass*
 Runtime::StdClass::subclass(std::string name)
 {
-  /* TODO */
+  Runtime::StdClass *objClass = Lang::Runtime::getObjectClass();
+  if(objClass->hasConstant(name))
+    return static_cast<Runtime::StdClass*>(objClass->getConstant(name));
+  return newSubclass(name);
 }
 
 int
 Runtime::StdClass::isSubclass(Runtime::StdClass *klass)
 {
-  /* TODO */
+  if(klass == this)                  return 1;
+  if(klass->getSuperClass() == 0)    return 0;
+  if(klass->getSuperClass() == this) return 1;
+  return isSubclass(klass->getSuperClass());
 }
 
 int
--- runtime/stdclass.hpp
+++ runtime/stdclass.hpp
@@ -13,7 +13,7 @@ namespace Runtime
   {
     private:
       std::string name;
-      std::map<std::string, Object> constants;
+      std::map<std::string, Object*> constants;
       std::map<std::string, std::string> methods;
       StdClass *superClass;
       void construct(std::string name);
@@ -22,18 +22,18 @@ namespace Runtime
       StdClass(std::string name, StdClass *superClass);
       StdClass(std::string name);
       std::string getName();
-      StdClass getSuperClass();
-      void     setConstant(std::string name, Object value);
+      StdClass* getSuperClass();
+      void     setConstant(std::string name, Object *value);
       Object*  getConstant(std::string name);
       int      hasConstant(std::string name);
       void     lookup(std::string name);
       int      hasMethod(std::string name);
       void     addMethod(std::string name, std::string method);
-      Object   newInstance(std::string value);
-      StdClass newSubclass(std::string name);
-      StdClass subclass(std::string name);
-      int      isSubclass(StdClass *klass);
-      int      equals(std::string other);
+      Object*  newInstance(std::string value);
+      StdClass* newSubclass(std::string name);
+      StdClass* subclass(std::string name);
+      int       isSubclass(StdClass *klass);
+      int       equals(std::string other);
   };
 
 }
