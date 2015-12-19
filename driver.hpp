7500008b48176a27eed8d0eac41137b5b997bfb0
Benjamin J Anderson <banderson@mashable.com>
2015-12-19 15:21:21 -0600

    Clean up driver debug code

--- driver.hpp
+++ driver.hpp
@@ -31,25 +31,12 @@ namespace FrontEnd{
     
        virtual ~Driver();
     
-       void parse( const char *filename );
-      
+       void parse( const char *filename );      
        void set_stack(std::vector<Nodes::AbstractNode*> stack);
        Runtime::Object* execute(Context *context);
-       void add_upper();
-       void add_lower();
-       void add_word( const std::string &word );
-       void add_class( const std::string &klass );
-       void add_newline();
-       void add_char();
-       void add_int( int &number );
     
-       std::ostream& print(std::ostream &stream);
+
     private:
-       int chars      = 0;
-       int words      = 0;
-       int lines      = 0;
-       int uppercase  = 0;
-       int lowercase  = 0;
        FrontEnd::Parser  *parser  = nullptr;
        FrontEnd::Scanner *scanner = nullptr;
     };
