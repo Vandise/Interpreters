4efd722a292659b1910a201c151142ca97f9a06f
Benjamin J Anderson <banderson@mashable.com>
2015-12-27 20:54:07 -0600

    Implement rough interactive mode

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <cstdlib>
 #include <map>
 
@@ -8,10 +9,28 @@
 int
 main( const int argc, const char **argv )
 {
-   if(argc != 2 )
-      return ( EXIT_FAILURE );
+  if(argc != 2 )
+    return ( EXIT_FAILURE );
 
-   Bootstrapper::run()->eval(argv[1]);
+  if(std::string(argv[1]) == "-i")
+  {
+    std::string input;
+    do
+    {
+      std::cout << "\n >> ";
+      //std::cin >> input;
+      std::getline(std::cin,input);
+      std::ofstream out("interactive.src");
+      out << input;
+      out.close();
+      std::cout << "=> ";
+      std::cout << Bootstrapper::run()->eval("interactive.src")->getValue();
+    } while(input != "exit");
+    std::cout << "\nInteractive mode\n";
+    return( EXIT_SUCCESS );
+  }
 
-   return( EXIT_SUCCESS );
+  Bootstrapper::run()->eval(argv[1]);
+
+  return( EXIT_SUCCESS );
 }
